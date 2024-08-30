55d5e7a9797ff0cae69e060a6492bf689f045b22
tuanminhphan45 <tuanminhphan45@gmail.com>
2024-08-30 14:02:50 +0700

    add full folder and fix mistake

--- USTH Weather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ USTH Weather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -2,52 +2,60 @@ package vn.edu.usth.usthweather;
 
 import android.os.Bundle;
 import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class WeatherActivity extends AppCompatActivity {
-
-    private static final String TAG = "WeatherActivity";
+    private static final String TAG = "Weather";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(TAG, "Create");
-
-        setContentView(R.layout.activity_weather); // Make sure this points to the correct layout
-
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
         ForecastFragment firstFragment = new ForecastFragment();
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.main, firstFragment)
-                .commit();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.main, firstFragment).commit();
+
+        Log.i(TAG, "On Create");
     }
 
     @Override
-    protected void onStart() {
+    protected void onStart(){
         super.onStart();
-        Log.i(TAG, "onStart");
+        Log.i(TAG, "On Start");
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume(){
         super.onResume();
-        Log.i(TAG, "onResume");
+        Log.i(TAG, "On Resume");
     }
 
     @Override
-    protected void onPause() {
+    protected void onPause(){
         super.onPause();
-        Log.i(TAG, "onPause");
+        Log.i(TAG, "On Pause");
     }
 
     @Override
-    protected void onStop() {
+    protected void onStop(){
         super.onStop();
-        Log.i(TAG, "onStop");
+        Log.i(TAG, "On Stop");
     }
 
     @Override
-    protected void onDestroy() {
+    protected void onDestroy(){
         super.onDestroy();
-        Log.i(TAG, "onDestroy");
+        Log.i(TAG, "On Destroy");
     }
 }
